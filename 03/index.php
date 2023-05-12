6289ad7c16d155e34114fe8050d823806eeaea4f
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2023-05-12 18:18:35 +0500

    added ojects lesson 3

--- 03/index.php
+++ 03/index.php
@@ -1,1 +1,21 @@
 <?php
+//методы объекта
+error_reporting(-1);
+require_once 'classes/Car.php';
+
+function debug($data){
+    echo '<pre>' . print_r($data,  1) . '</pre>';
+}
+
+$car1 = new Car();
+$car1->collor = 'black';
+$car1->brand = 'Volvo';
+$car1->speed = 260;
+
+$car2 = new Car();
+$car2->collor = 'red';
+$car2->brand = 'BMW';
+$car2->speed = 320;
+
+echo $car1->getCarInfo();
+echo $car2->getCarInfo();
