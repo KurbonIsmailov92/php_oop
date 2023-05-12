10bab1df6b75076b7b0dbb8761f4b8e80fa96b64
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2023-05-12 18:19:43 +0500

    added ojects lesson 3

--- 03/classes/Car.php
+++ 03/classes/Car.php
@@ -3,7 +3,15 @@
 class Car
 {
     public $collor;
-    public $wheels  = 4;
-    public $speed   = 180;
+    public $wheels = 4;
+    public $speed = 180;
     public $brand;
+
+    public function getCarInfo()
+    {
+        echo "<h3> About my Auto </h3>
+                Brand: {$this->brand}<br>
+                Count of wheels: {$this->wheels}<br>
+                Speed: {$this->speed}<br>";
+    }
 }
