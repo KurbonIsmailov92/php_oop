effa2440c1886d24681374c9977763349e3ec667
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2023-05-24 13:08:08 +0500

    homework - work with files in oop

---
+++ 05/index.php
@@ -0,0 +1,10 @@
+<?php error_reporting(-1);
+//homework
+require_once 'classes/File.php';
+
+$file = new File(__DIR__ . '/file.txt');
+$file->write('Строка 1');
+$file->write('Строка 2');
+$file->write('Строка 3');
+$file->write('Строка 4');
+$file->write('Строка 5');
